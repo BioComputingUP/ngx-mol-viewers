75aa152a561797385e32e48fcabafd9db608215e
Damiano Clementel <damiano.clementel@gmail.com>
2024-02-02 17:44:34 +0100

    Updated representation using custom functions

--- projects/ngx-features-viewer/src/lib/features/dssp.ts
+++ projects/ngx-features-viewer/src/lib/features/dssp.ts
@@ -1,7 +1,14 @@
 import Feature from "./feature";
+import * as I from "./loci";
+
+// Define DSSP locus
+interface Locus extends I.Locus {
+  // Define available DSSP codes
+  code: 'G' | 'H' | 'I' | 'E' | 'B' | 'S' | 'T' | 'C' | '-';
+}
 
 // NOTE It uses DSSP values { helix: G/H/I, strand: E/B, loop: S/T/C, undefined: - }
-export default interface DSSP extends Feature<'G' | 'H' | 'I' | 'E' | 'B' | 'S' | 'T' | 'C' | '-'> {
+export default interface DSSP extends Feature<Locus> {
   // Override type
   type: 'dssp';
 }
--- projects/ngx-features-viewer/src/lib/features/pins.ts
+++ projects/ngx-features-viewer/src/lib/features/pins.ts
@@ -1,6 +1,9 @@
 import Feature from "./feature";
+import { Locus } from "./loci";
 
-export default interface Pins extends Feature<boolean> {
+export type Pin = Pick<Locus, 'start' | 'color'>;
+
+export default interface Pins extends Feature<Pin> {
     // Override type
     type: 'pins';
 }
