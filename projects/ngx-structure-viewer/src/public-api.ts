e32a867042caf1f9d0ba51f0af127c891d2e375e
Damiano Clementel <damiano.clementel@gmail.com>
2024-04-08 11:08:15 +0100

    Now structure viewer export structure service for structural data retrieval

--- projects/ngx-structure-viewer/src/public-api.ts
+++ projects/ngx-structure-viewer/src/public-api.ts
@@ -1,5 +1,8 @@
 export { NgxStructureViewerComponent } from './lib/ngx-structure-viewer.component';
+// Export interfaces
 export { Interaction, Interactor } from './lib/interfaces/interaction';
 export { Settings } from './lib/interfaces/settings';
 export { Source } from './lib/interfaces/source';
 export { Locus } from './lib/interfaces/locus';
+// Export services
+export { StructureService } from './lib/services/structure.service';
