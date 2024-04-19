86a47ebfddc0e6919f6523f53c338dec9ae330ab
Damiano Clementel <damiano.clementel@gmail.com>
2024-04-19 11:03:51 +0100

    Introduces React UI rendering. Does it make sense to call React from within Angular? No. DO they want it? Yes.

--- projects/ngx-structure-viewer/src/lib/services/plugin.service.ts
+++ projects/ngx-structure-viewer/src/lib/services/plugin.service.ts
@@ -1,18 +1,25 @@
-import { DefaultPluginSpec } from 'molstar/lib/mol-plugin/spec';
-import { PluginContext } from 'molstar/lib/mol-plugin/context';
+import { DefaultPluginUISpec } from 'molstar/lib/mol-plugin-ui/spec';
+import { PluginUIContext } from 'molstar/lib/mol-plugin-ui/context';
+import { PluginConfig } from 'molstar/lib/mol-plugin/config';
+// import { PluginContext } from 'molstar/lib/mol-plugin/context';
 import { Observable, from, map, shareReplay } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'platform' })
 export class PluginService {
 
-  readonly plugin!: PluginContext;
+  readonly plugin!: PluginUIContext;
 
-  readonly plugin$: Observable<PluginContext>;
+  readonly plugin$: Observable<PluginUIContext>;
 
   constructor() {
     // Initialize plugin context
-    this.plugin = new PluginContext(DefaultPluginSpec());
+    this.plugin = new PluginUIContext({
+      ...DefaultPluginUISpec(),
+      config: [
+          [PluginConfig.VolumeStreaming.Enabled, false]
+      ]
+    });
     // Define plugin initialization pipeline
     this.plugin$ = from(this.plugin.init()).pipe(
       // Get current plugin instance
