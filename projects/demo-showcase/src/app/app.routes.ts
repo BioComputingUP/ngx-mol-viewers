35272f676d2d26be8563be27e3de1d912ba9ea0c
Damiano Clementel <damiano.clementel@gmail.com>
2024-06-15 14:30:31 +0200

    Implemented lazy-loading for pages. This avoids hitting the bundle budget limits

--- projects/demo-showcase/src/app/app.routes.ts
+++ projects/demo-showcase/src/app/app.routes.ts
@@ -1,13 +1,22 @@
-import { PageStructureViewerComponent } from './page-structure-viewer/page-structure-viewer.component';
-import { PageSequenceViewerComponent } from './page-sequence-viewer/page-sequence-viewer.component';
-import { PageFeaturesViewerComponent } from './page-features-viewer/page-features-viewer.component';
+// import { PageStructureViewerComponent } from './page-structure-viewer/page-structure-viewer.component';
+// import { PageSequenceViewerComponent } from './page-sequence-viewer/page-sequence-viewer.component';
+// import { PageFeaturesViewerComponent } from './page-features-viewer/page-features-viewer.component';
 import { PageHomeComponent } from './page-home/page-home.component';
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: 'structure', component: PageStructureViewerComponent },
-    { path: 'sequence', component: PageSequenceViewerComponent },
-    { path: 'feature', component: PageFeaturesViewerComponent },
+    { 
+        path: 'structure', 
+        loadComponent: () => import('./page-structure-viewer/page-structure-viewer.component').then(m => m.PageStructureViewerComponent) 
+    },
+    { 
+        path: 'sequence', 
+        loadComponent: () => import('./page-sequence-viewer/page-sequence-viewer.component').then(m => m.PageSequenceViewerComponent) 
+    },
+    { 
+        path: 'features', 
+        loadComponent: () => import('./page-features-viewer/page-features-viewer.component').then(m => m.PageFeaturesViewerComponent) 
+    },
     { path: '', component: PageHomeComponent },
     { path: '**', redirectTo: '' }
 ];
