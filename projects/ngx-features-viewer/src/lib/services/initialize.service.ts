78cd372a80a9f7f1ad8cbad916ad4512ce70b0d3
Adel Bouhraoua <adelbouhraoua23@gmail.com>
2026-08-05 16:09:41 +0200

    fix(feature-viewer):prevent default wheel event after reaching zoom limit - adjust boundaries of features and shadow boxes on seleceted features   to include residues in the range

--- projects/ngx-features-viewer/src/lib/services/initialize.service.ts
+++ projects/ngx-features-viewer/src/lib/services/initialize.service.ts
@@ -67,7 +67,7 @@ export class InitializeService implements OnDestroy {
   public focusMousedown!: (
     this: SVGGElement,
     event: unknown,
-    d: undefined,
+    d: undefined
   ) => void;
 
   // Get referenced HTML div
@@ -176,11 +176,13 @@ export class InitializeService implements OnDestroy {
   public hoverCircleMarker!: Circle;
 
   // Declare initialization pipeline
-  public readonly initialized$ = new ReplaySubject<d3.Selection<SVGSVGElement, undefined, null, undefined>>(1);
+  public readonly initialized$ = new ReplaySubject<
+    d3.Selection<SVGSVGElement, undefined, null, undefined>
+  >(1);
 
   public getCoordinates(
     mouseEvent: MouseEvent,
-    traceId: unknown,
+    traceId: unknown
   ): [number, number] {
     // Define coordinates
     const x = Math.floor(this.scale.x.invert(mouseEvent.offsetX) + 0.5);
@@ -275,6 +277,11 @@ export class InitializeService implements OnDestroy {
       .call(this.zoom)
       .on('dblclick.zoom', () => this.zoom.scaleTo(this.focus, 1));
 
+    // Prevent page scroll when reaching zoom limits
+    // https://github.com/d3/d3-zoom/issues/158
+    this.focus.call(this.zoom).on('wheel', (event: WheelEvent) => {
+      event.preventDefault();
+    });
     // Save the mousedown.zoom event listener
     this.focusMousedown = this.focus.on('mousedown.zoom')!;
 
@@ -332,7 +339,7 @@ export class InitializeService implements OnDestroy {
           this.svg
             .select('#plot-background')
             .attr('fill', settings['plot-background-color']);
-        }),
+        })
       )
       .subscribe();
   }
@@ -343,7 +350,7 @@ export class InitializeService implements OnDestroy {
 }
 
 export function checkContentSettings(
-  contentSettings: Partial<ContentSettings> | undefined,
+  contentSettings: Partial<ContentSettings> | undefined
 ) {
   if (contentSettings) {
     if (contentSettings['line-height'] && contentSettings['line-height'] < 0) {
@@ -364,7 +371,7 @@ export function checkContentSettings(
       contentSettings['content-size'] > contentSettings['line-height']
     ) {
       console.warn(
-        'Content size cannot be bigger than line height, setting to line height',
+        'Content size cannot be bigger than line height, setting to line height'
       );
       contentSettings['content-size'] = contentSettings['line-height'];
     }
