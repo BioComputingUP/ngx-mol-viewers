0b6076eb7cefde0168e410a7be58b6f386c30d3f
Adel Bouhraoua <adelbouhraoua23@gmail.com>
2026-08-12 15:52:39 +0200

    refactor: draw service debloat. split logic accross GridRenderer and SequenceRenderer
    (cherry picked from commit d8aef6b5738e95f1351bffea6a55f04243ade422)

--- projects/ngx-features-viewer/src/lib/services/draw.service.spec.ts
+++ projects/ngx-features-viewer/src/lib/services/draw.service.spec.ts
@@ -1,12 +1,25 @@
 import { TestBed } from '@angular/core/testing';
-
 import { DrawService } from './draw.service';
+import { InitializeService } from './initialize.service';
+import { FeaturesService } from './features.service';
+import { TooltipService } from './tooltip.service';
+import { SequenceRenderer } from './renderers/sequence.renderer';
+import { GridRenderer } from './renderers/grid.renderer';
 
 describe('DrawService', () => {
   let service: DrawService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        DrawService,
+        InitializeService,
+        FeaturesService,
+        TooltipService,
+        SequenceRenderer,
+        GridRenderer,
+      ]
+    });
     service = TestBed.inject(DrawService);
   });
 
