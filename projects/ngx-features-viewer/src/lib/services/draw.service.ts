a58caf3217f73a5758bd9879c014f5fdb78be455
Adel Bouhraoua <adelbouhraoua23@gmail.com>
2026-08-12 15:52:39 +0200

    refactor: draw service debloat. split logic accross GridRenderer and SequenceRenderer

--- projects/ngx-features-viewer/src/lib/services/draw.service.ts
+++ projects/ngx-features-viewer/src/lib/services/draw.service.ts
@@ -21,6 +21,8 @@ import { FeaturesService } from './features.service';
 // Services
 import { InitializeService, SelectionContext } from './initialize.service';
 import { TooltipService } from './tooltip.service';
+import { SequenceRenderer } from './renderers/sequence.renderer';
+import { GridRenderer } from './renderers/grid.renderer';
 
 
 
@@ -55,18 +57,9 @@ export class DrawService {
 
   public readonly selectedFeature$: Observable<SelectionContext | undefined>;
 
-  public 'group.residues'!: SequenceContainer;
-
-  public 'group.dots'!: SequenceContainer;
-
   public 'group.labels'!: LabelGroup;
 
   public 'group.traces'!: TraceGroup;
-
-  public 'group.grid'!: GridLines;
-
-  public sequenceCharWidth = 0.0;
-  public featureLabelCharWidth = 0.0;
 
   public tooltip!: d3.Selection<HTMLDivElement, unknown, null, unknown>;
 
@@ -92,7 +85,9 @@ export class DrawService {
   constructor(
     private initializeService: InitializeService,
     private featuresService: FeaturesService,
-    private tooltipService: TooltipService
+    private tooltipService: TooltipService,
+    private sequenceRenderer: SequenceRenderer,
+    private gridRenderer: GridRenderer
   ) {
     // Define draw initialization
     this.draw$ = combineLatest([
@@ -108,9 +103,9 @@ export class DrawService {
         // Update horizontal scale
         x.domain(domain);
       }),
-      tap(() => this.calculateCharWidth()),
+      tap(() => this.sequenceRenderer.calculateCharWidth()),
       // Draw sequence
-      map(([, sequence]) => this.createSequence(sequence)),
+      map(([, sequence]) => this.sequenceRenderer.createSequence(sequence)),
       // Initialize brush region
       tap(() => this.createBrush()),
       // Initialize tooltip
@@ -122,7 +117,7 @@ export class DrawService {
       // Update vertical scale
       tap((traces: InternalTraces) => {
         this.updateScale(traces);
-        this.createGrid(traces);
+        this.gridRenderer.createGrid(traces);
         return;
       }),
 
@@ -135,9 +130,9 @@ export class DrawService {
       this.initializeService.settings$,
     ]).pipe(
       // Move sequence residues in correct position
-      tap(() => this.updateSequence()),
+      tap(() => this.sequenceRenderer.updateSequence()),
       // Move grid in correct position
-      tap(() => this.updateGrid()),
+      tap(() => this.gridRenderer.updateGrid()),
       // Render traces (creates and updates)
       tap(([traces]) => this.renderTraces(traces)),
 
@@ -179,45 +174,6 @@ export class DrawService {
       }),
       shareReplay(1)
     );
-  }
-
-  private calculateCharWidth() {
-    const settings = this.initializeService.settings;
-    // Get the width of the character 'A' in the sequence
-    const text = this.initializeService.draw
-      .append('text')
-      .attr('class', 'sequence')
-      .text('A');
-    const bbox = text.node()!.getBBox();
-    this.sequenceCharWidth = bbox.width;
-    text.remove();
-
-    // Get the width of the character 'A' in the feature label
-    const text2 = this.initializeService.draw
-      .append('text')
-      .attr('class', 'feature')
-      .text('A');
-    const bbox2 = text2.node()!.getBBox();
-    this.featureLabelCharWidth = bbox2.width;
-    text2.remove();
-
-    // Get the width of the character 'A' in the x-axis
-    const text3 = this.initializeService.draw
-      .append('text')
-      .attr('class', 'tick')
-      .text('A');
-    const bbox3 = text3.node()!.getBBox();
-    const xAxisXCharHeight = bbox3.height;
-    text3.remove();
-
-    // Update the margin bottom to accommodate at least the height of the character of the x-axis
-    if (settings['x-axis-show'] !== false) {
-      const settings = this.initializeService.settings;
-      settings['margin-bottom'] = Math.max(
-        settings['margin-bottom'],
-        xAxisXCharHeight + 6
-      );
-    }
   }
 
   // Update vertical scale
@@ -271,146 +227,6 @@ export class DrawService {
     this.tooltip = this.tooltipService._tooltip;
   }
 
-  private createSequence(sequence: Sequence) {
-    // Initialize residues group
-    const group = this.initializeService.draw
-      // Select previous residues group
-      .selectAll('g.sequence')
-      // Bind residues group to sequence
-      .data<Sequence>([sequence])
-      // Create current residues group
-      .join('g')
-      .attr('class', 'sequence');
-
-    // Create residues container inside the sequence group
-    this['group.residues'] = group.append('g').attr('class', 'residues');
-
-    this['group.dots'] = group.append('g').attr('class', 'dots');
-  }
-
-  private updateSequence() {
-    // Get the sequence and from the sequence the residues
-    const sequence = this.initializeService.sequence;
-    const settings = this.initializeService.settings;
-    const residues = parseSequence(sequence);
-
-    // The list of residues can be empty in the case the sequence is a length only
-    if (residues.length === 0 || settings['sequence-show'] === false) {
-      return;
-    }
-
-    // Get scale (x, y axis)
-    const { x, y } = this.initializeService.scale;
-    // Get line height
-    const lh = this.initializeService.settings['line-height'];
-    const cs = this.initializeService.settings['content-size'];
-    // Define container/cell width and (maximum) text width
-    const cellWidth = x(1) - x(0);
-    // Get maximum character width
-    const charWidth = this.sequenceCharWidth;
-    // Define residues group
-    const residuesContainer = this['group.residues'];
-    const dotsContainer = this['group.dots'];
-
-    const domainStart = x.domain()[0] + 0.5;
-    const domainEnd = x.domain()[1];
-
-    if (charWidth + 0.5 > cellWidth) {
-      // Remove residues if dots are to be shown
-      residuesContainer.selectAll('*').remove();
-
-      // Calculate number of dots needed
-      const domainLength = domainEnd - domainStart;
-
-      // Calculate how many cells are needed for each dot
-      const spacing = 2;
-      const bits = (domainLength * cellWidth) / charWidth / spacing;
-      const bitSize = domainLength / bits + 1;
-
-      const xPositions = d3
-        .range(domainStart, domainEnd, bitSize)
-        .map((i) => x(i + bitSize / 2));
-
-      // Create or update dots
-      dotsContainer
-        .selectAll('text.dot')
-        .data(xPositions)
-        .join('text')
-        .attr('class', 'dot')
-        .text('.')
-        .attr('x', (d) => d)
-        .attr('y', y('sequence') + lh / 2)
-        .attr('width', charWidth)
-        .attr('height', lh)
-        .attr('dominant-baseline', 'central')
-        .style('text-anchor', 'middle');
-    } else {
-      // Ensure dots are removed if residues are to be shown
-      this['group.dots'].selectAll('*').remove();
-
-      const domainStartFloor = Math.floor(domainStart + 0.5);
-      const domainEndCeil = Math.min(Math.ceil(domainEnd), residues.length);
-
-      const visibleResidues = residues.slice(
-        Math.max(0, domainStartFloor - 1),
-        domainEndCeil
-      );
-
-      // Create the visible residues inside the residues container as rect with the color of the residue
-      if (settings['sequence-background-color']) {
-        const color = (d: string) =>
-          sequenceColors[settings['sequence-background-color']!][d as never] ||
-          sequenceColors[settings['sequence-background-color']!].X;
-        let height;
-        let yValue: number = y('sequence');
-
-        switch (settings['sequence-background-height']) {
-          case '100%':
-            height = '100%';
-            break;
-          case 'content-size':
-            height = cs;
-            yValue += (lh - cs) / 2;
-            break;
-          case 'line-height':
-            height = lh;
-            break;
-          default:
-            height = cs;
-        }
-
-        residuesContainer
-          .selectAll('rect.residue')
-          .data(visibleResidues)
-          .join('rect')
-          .attr('class', 'residue')
-          .attr('x', (d, i) => x(i + domainStartFloor - 0.5))
-          .attr('y', yValue)
-          .attr('width', cellWidth)
-          .attr('height', height)
-          .attr('fill', color)
-          .attr('fill-opacity', settings['sequence-background-opacity'] || 0.5);
-      }
-
-      // Create the visible residues inside the residues container as text elements
-      residuesContainer
-        .selectAll('text.residue')
-        .data(visibleResidues)
-        .join('text')
-        .attr('class', 'residue')
-        .text((d) => '' + d)
-        .attr('x', (d, i) => x(i + domainStartFloor))
-        .attr('y', y('sequence') + lh / 2)
-        .attr('dominant-baseline', 'central')
-        .style('text-anchor', 'middle');
-    }
-
-    const textColor = settings['text-color'];
-    // Update the residue and dots color always
-    residuesContainer.selectAll('text.residue').attr('fill', textColor);
-    dotsContainer.selectAll('text.dot').attr('fill', textColor);
-  }
-
   private createBrush() {
     this.initializeService.brushRegion = this.initializeService.draw
       .append('g')
@@ -443,115 +259,13 @@ export class DrawService {
       .attr('width', 0);
   }
 
-  private createGrid(traces: InternalTraces): void {
-    const group = this.initializeService.focus
-      // Create parent grid element
-      .selectAll<SVGGElement, InternalTraces>('g.grid')
-      .data<InternalTraces>([traces], index)
-      .join('g')
-      .attr('class', 'grid')
-      .lower();
-
-    this['group.grid'] = group
-      .selectAll<SVGGElement | BaseType, InternalTrace>('g.grid-line-group')
-      .data<InternalTrace>(traces, identity)
-      .join('g')
-      .attr('id', (d) => 'grid-' + d.id)
-      .attr('class', 'grid-line-group')
-      .join('line');
-
-    this['group.grid'].each((trace) => {
-      if (trace.options?.['grid']) {
-        // In each group of grid lines, create the lines
-        this['group.grid']
-          .selectAll('line.grid-line')
-          .data(trace.options?.['grid-y-values'] || [])
-          .enter()
-          .append('line')
-          .attr('class', 'grid-line')
-          .style('shape-rendering', 'crispedges')
-          .attr('id', (d, index) => 'grid-line-' + index);
-      }
-
-      // Create initial zero-line if defined
-      if (trace.options?.['zero-line']) {
-        // Create zero line
-        this['group.grid']
-          .selectAll('line.zero-line')
-          .data([true])
-          .enter()
-          .append('line')
-          .attr('class', 'zero-line')
-          .style('shape-rendering', 'crispedges')
-          .attr('id', 'zero-line');
-      }
-    });
-  }
-
-  private updateGrid(): void {
-    const group: GridLines = this['group.grid'];
-
-    const y = this.initializeService.scale.y;
-    const settings = this.initializeService.settings;
-    const x1 = this.initializeService.x1;
-    const x2 = this.initializeService.x2;
-
-    group.each(function (trace: InternalTrace) {
-      const traceGroup = d3.select(this);
-
-      // Get all the necessary values to compute the position of the grid lines
-      const mt = y('' + trace.id);
-      const lh = trace.options?.['line-height'] || settings['line-height'];
-      const cs = trace.options?.['content-size'] || settings['content-size'];
-
-      // top is calculated as the distance to the top, plus the lh/2 to get the mid-point of the line, plus the cs/2 to get the bottom of the line
-      const bottom = mt + lh / 2 + cs / 2;
-      const top = mt + lh / 2 - cs / 2;
-
-      function rescaleY(yValue: number): number {
-        // top and bottom are actually switched, as the y-axis is inverted
-        return (
-          bottom +
-          ((yValue - trace.domain.min) /
-            (trace.domain.max - trace.domain.min)) *
-            (top - bottom)
-        );
-      }
-
-      // Update grid lines
-      traceGroup
-        .selectAll('line.grid-line')
-        .data(trace.options?.grid ? trace.options?.['grid-y-values'] || [] : [])
-        .attr('x1', x1)
-        .attr('x2', x2)
-        .attr('y1', (d) => rescaleY(d))
-        .attr('y2', (d) => rescaleY(d))
-        .attr(
-          'stroke',
-          trace.options?.['grid-line-color'] || settings['grid-line-color']
-        )
-        .attr('stroke-width', trace.options?.['grid-line-width'] || 1);
-
-      // Update zero-line if defined
-      traceGroup
-        .selectAll('line.zero-line')
-        .data(trace.options?.['zero-line'] ? [true] : [])
-        .attr('x1', x1)
-        .attr('x2', x2)
-        .attr('y1', rescaleY(0))
-        .attr('y2', rescaleY(0))
-        .attr('stroke', trace.options?.['zero-line-color'] || 'black')
-        .attr('stroke-width', trace.options?.['zero-line-width'] || 1);
-    });
-  }
-
   private renderTraces(traces: InternalTraces): void {
     const scale = this.initializeService.scale;
     const settings = this.initializeService.settings;
     const currentDomainStart = scale.x.domain()[0];
     const currentDomainEnd = scale.x.domain()[1];
     const featureSortingFunc = settings?.['sort-nested-locuses'] ? sortLocuses : undefined;
-    const charWidth = this.featureLabelCharWidth;
+    const charWidth = this.sequenceRenderer.featureLabelCharWidth;
     const cw = scale.x(1) - scale.x(0);
     const coilPoints = this.coilPoints;
 
@@ -579,7 +293,7 @@ export class DrawService {
           exit => exit.remove()
         )
         .call(g => featureSortingFunc ? g.sort(featureSortingFunc) : g)
-        .call(g => this.bindFeatureEvents(g, trace))
+        .call(g => this.tooltipService.bindFeatureEvents(g, trace, this.selectedFeatureEmit$))
         .each((feature, featureIdx, featureNodes) => {
           // Calculate trace-specific layout values
           const mt = scale.y('' + trace.id) || 0;
@@ -637,58 +351,6 @@ export class DrawService {
       .attr('id', (_, i) => `trace-${trace.id}-feature-${i}`);
 
     return g;
-  }
-
-  private bindFeatureEvents(
-    selection: d3.Selection<SVGGElement, Feature, SVGGElement | d3.BaseType, unknown>,
-    trace: InternalTrace
-  ): void {
-    const tooltipService = this.tooltipService;
-    const initializeService = this.initializeService;
-    const selectionEmitter$ = this.selectedFeatureEmit$;
-    const circle = this.initializeService.hoverCircleMarker;
-    const scale = initializeService.scale;
-
-    selection.on('mouseenter', (event: MouseEvent, feature: Feature) => {
-      const featureIdx = trace.features.indexOf(feature);
-      tooltipService.onMouseEnter(event, trace, feature, featureIdx);
-    });
-
-    selection.on('mousemove', (event: MouseEvent, feature: Feature) => {
-      const featureIdx = trace.features.indexOf(feature);
-      tooltipService.onMouseMove(event, trace, feature, featureIdx);
-
-      if (feature.type === 'continuous') {
-        const coordinates = initializeService.getCoordinates(event, trace.id);
-        if (coordinates[0] <= 0.5) return;
-
-        const mt = scale.y('' + trace.id) || 0;
-        const lh = trace.options?.['line-height'] || initializeService.settings['line-height'];
-        const cs = trace.options?.['content-size'] || initializeService.settings['content-size'];
-        const center = mt + lh / 2;
-        const bottom = center + cs / 2;
-        const top = center - cs / 2;
-
-        const rescaleY = (yValue: number) => (
-          bottom +
-          ((yValue - trace.domain.min) / (trace.domain.max - trace.domain.min)) * (top - bottom)
-        );
-
-        circle
-          .attr('cx', scale.x(coordinates[0]))
-          .attr('cy', rescaleY((feature as any).values[coordinates[0] - 1]))
-          .attr('display', 'block');
-      }
-    });
-
-    selection.on('mouseleave', () => {
-      tooltipService.onMouseLeave();
-      circle.attr('display', 'none');
-    });
-
-    selection.on('click', (event: MouseEvent, feature: Feature) => {
-      selectFeature(feature, initializeService, event, trace, selectionEmitter$);
-    });
   }
 
   private updateShadowPosition() {
@@ -797,26 +459,4 @@ function getStartEndPositions(feature: Feature) {
       featureEnd = 10;
   }
   return { featureStart, featureEnd };
-}
-
-function selectFeature(
-  feature: Feature,
-  initializeService: InitializeService,
-  event: MouseEvent,
-  trace: InternalTrace,
-  selectionEmitter$: EventEmitter<SelectionContext | undefined>
-) {
-  let { featureStart, featureEnd } = getStartEndPositions(feature);
-
-  const coordinates = initializeService.getCoordinates(event, trace.id);
-  if (feature.type === 'continuous') {
-    featureStart = coordinates[0] - 0.5;
-    featureEnd = coordinates[0] + 0.5;
-  }
-  const selectionContext: SelectionContext = {
-    trace,
-    feature,
-    range: { start: featureStart, end: featureEnd },
-  };
-  selectionEmitter$.next(selectionContext);
 }
